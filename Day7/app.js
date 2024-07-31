2afe0e6e880fd7bcbc6ad54a9a3677bc48d30888
abhishek <rajabhishek7654@gmail.com>
2024-07-31 09:38:52 +0530

    routes

--- Day7/app.js
+++ Day7/app.js
@@ -1,17 +1,20 @@
+const PORT=8080;
+// PORT=process.env.PORT;
+
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 
 const express = require('express');
 const app = express();
 
-const logger=require('morgan');
+const logger = require('morgan');
 const indexRouter = require('./routes/indexRouter');
 
 app.use(logger('tiny'));
 app.use('/', indexRouter);
 
-app.listen(process.env.PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-}
-);
 
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
