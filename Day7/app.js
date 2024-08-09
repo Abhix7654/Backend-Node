219e4f1346278889d10b5149bb54dce300cb8997
abhishek <rajabhishek7654@gmail.com>
2024-08-09 08:50:57 +0530

    validation

--- Day7/app.js
+++ Day7/app.js
@@ -1,24 +1,22 @@
-const PORT=8080;
-// PORT=process.env.PORT;
-
 const dotenv = require('dotenv');
-dotenv.config({ path: './.env' });
+dotenv.config({ path: './Day7/.env' }); 
+
 const path = require('path');
-
-
 const express = require('express');
-const app = express();
-
 const logger = require('morgan');
 const indexRouter = require('./routes/indexRouter');
+
+const app = express();
+
+
+const PORT = process.env.PORT ;
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(logger('tiny'));
 app.use('/', indexRouter);
-
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
--- Day7/routes/indexRouter.js
+++ Day7/routes/indexRouter.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+
+
+let user=[{name:"abhishek",age:21,college:"TiT"},{name:"muskan",age:20,college:"TiT"},{name:"nitesh",age:25,college:"TiT"}]
+
 router.get('/', (req, res) => {
     // res.status(200).json({ message: "Hello From Homepage" });
-    res.render('index',{title: 'Homepage',n:n});
+    res.render("index", { title: "My Page Title",user: user});
 });
 
 module.exports = router;
